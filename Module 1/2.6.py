0dbc5ca50eb1029269f67d40ebf0175f4cd8f834
nirmanirajakaruna <nirmanirajakaruna217@gmail.com>
2025-09-21 01:32:48 +0530

    module 1 question 2.6 is finished

---
+++ Module 1/2.6.py
@@ -0,0 +1,5 @@
+import random
+code = [random.randint(0, 9) for _ in range(3)]
+code1 = [random.randint(1, 6) for _ in range(4)]
+print("Three digit code : ", code)
+print("Four digit code : ", code1)
