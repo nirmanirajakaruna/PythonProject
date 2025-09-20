8f11554dad0c7ac09f051895ca05a56164f05c0e
nirmanirajakaruna <nirmanirajakaruna217@gmail.com>
2025-09-21 01:00:54 +0530

    module 1 question 2.4 is finished

---
+++ Module 1/2.4.py
@@ -0,0 +1,9 @@
+a = int(input("Enter First Number : "))
+b = int(input("Enter Second Number : "))
+c = int(input("Enter Third Number : "))
+sum = a + b + c
+product = a * b * c
+average = float(sum / 3)
+print(f"The sum of {a} and {b} and {c} is {sum}")
+print(f"The product of {a} and {b} and {c} is {product}")
+print(f"The average of {a} and {b} and {c} is {average}")
