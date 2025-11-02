0ccc2725cade3ea215d6b1282febdf240654709b
nirmanirajakaruna <nirmanirajakaruna217@gmail.com>
2025-11-02 16:31:01 +0530

    module 11 question 1 is finished

---
+++ Module 11/11.1.py
@@ -0,0 +1,33 @@
+class Publication:
+    def __init__(self, name):
+        self.name = name
+
+    def print_information(self):
+        print(f"Publication Name: {self.name}")
+
+class Book(Publication):
+    def __init__(self, name, author, page_count):
+        super().__init__(name)
+        self.author = author
+        self.page_count = page_count
+
+    def print_information(self):
+        print(f"Book Name: {self.name}")
+        print(f"Author: {self.author}")
+        print(f"Page Count: {self.page_count}")
+
+class Magazine(Publication):
+    def __init__(self, name, chief_editor):
+        super().__init__(name)
+        self.chief_editor = chief_editor
+
+    def print_information(self):
+        print(f"Magazine Name: {self.name}")
+        print(f"Chief Editor: {self.chief_editor}")
+
+magazine = Magazine("Donald Duck", "Aki Hyypä")
+book = Book("Compartment No. 6", "Rosa Liksom", 192)
+
+magazine.print_information()
+print()
+book.print_information()
